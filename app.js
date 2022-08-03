7db6a4b9733442271a2d37c29bb879a939c83c9c
ShaneCodes <sfriedman7410@gmail.com>
2022-08-02 22:31:00 -0700

    salt/hash authentication with passport mongoose

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 const session = require('express-session'); //sessions set up
 const FileStore = require('session-file-store')(session); //keep track of sessions
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -32,25 +34,23 @@ app.use(session({
 	store: new FileStore()
 }));
 
+//for using session based authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 //this is where I added authentication
 function auth(req, res, next) {
-	console.log(req.session);
+	console.log(req.user);
 
-  	if (!req.session.user) {
+  	if (!req.user) {
 		const err = new Error('You are not authenticated!');
 		err.status = 401;
 		return next(err);
   	} else {
-		if (req.session.user === 'authenticated') {
-			return next(); //checking to see if cookie value = authenticated, if so grant access
-		} else {
-			const err = new Error('You are not authenticated!');
-			err.status = 401;
-			return next(err);
-		}
+		return next();
 	}
 }
 app.use(auth);
