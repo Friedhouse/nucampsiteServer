c2d8216488f2daf78dd0a31925ba2f645e4439c4
ShaneCodes <sfriedman7410@gmail.com>
2022-08-02 21:38:41 -0700

    session authentication implimented

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 const session = require('express-session'); //sessions set up
@@ -13,6 +12,7 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+
 
 var app = express();
 
@@ -32,34 +32,20 @@ app.use(session({
 	store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 //this is where I added authentication
 function auth(req, res, next) {
 	console.log(req.session);
 
   	if (!req.session.user) {
-		const authHeader = req.headers.authorization;
-		if (!authHeader) {
-			const err = new Error('You are not authenticated!');
-			res.setHeader('WWW-Authenticate', 'Basic');
-			err.status = 401;
-			return next(err);
-		}
-
-		const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
-		const user = auth[0];
-		const pass = auth[1];
-		if (user === 'admin' && pass === 'password') {
-			req.session.user =  'admin'; //assigning cookie data to user
-			return next(); //authorized
-		} else {
-			const err = new Error('You are not authenticated!');
-			res.setHeader('WWW-Authenticate', 'Basic');      
-			err.status = 401;
-			return next(err);
-		}
+		const err = new Error('You are not authenticated!');
+		err.status = 401;
+		return next(err);
   	} else {
-		if (req.session.user === 'admin') {
-			return next(); //checking to see if cookie value = admin, if so grant access
+		if (req.session.user === 'authenticated') {
+			return next(); //checking to see if cookie value = authenticated, if so grant access
 		} else {
 			const err = new Error('You are not authenticated!');
 			err.status = 401;
@@ -71,8 +57,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
